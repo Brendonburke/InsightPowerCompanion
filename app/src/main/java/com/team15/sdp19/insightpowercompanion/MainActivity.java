50fd96f7116db157788481d9b51858bef43fc3cf
Brendon <burkebrendon@gmail.com>
2018-11-15 16:01:15 -0500

    Real-Time graph!!! (random numbers)

--- app/src/main/java/com/team15/sdp19/insightpowercompanion/MainActivity.java
+++ app/src/main/java/com/team15/sdp19/insightpowercompanion/MainActivity.java
@@ -1,4 +1,5 @@
 package com.team15.sdp19.insightpowercompanion;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -8,20 +9,60 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.ArrayDeque;
 import java.util.Queue;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 /* Main
 * TODO Real-time Graphing
 * TODO Add Button Interaction
 * TODO Update TextView Field*/
 public class MainActivity extends AppCompatActivity {
+    private int time = 0;
     private LineGraphSeries<DataPoint> series;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GraphView graph = findViewById(R.id.graph);
+        series = new LineGraphSeries<>();
+        graph.addSeries(series);
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(0);
+        graph.getViewport().setMaxX(10);
+        graph.getViewport().setScrollable(true);
+    }
 
-        double time =-10;
+    @Override
+    protected void onResume(){
+        super.onResume();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true){
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            addPoint();
+
+                        }
+                    });
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).start();
+    }
+    private void addPoint() {
+        series.appendData(new DataPoint(time++, new Random().nextDouble() * 10d),true,100);
+    }
+    private double powerVal() {
+        return 0.0;
+    }
+}
+    /*double time =-10;
         Queue<Double> y = new ArrayDeque<Double>();
 
         for(double i=0;i<100;i++){
@@ -32,19 +73,18 @@ public class MainActivity extends AppCompatActivity {
             else y.offer(i);
         }
         Object[] data = y.toArray();
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        graph.getViewport().setXAxisBoundsManual(true);
-        graph.getViewport().setMinX(-10);
-        graph.getViewport().setMaxX(0);
-        series = new LineGraphSeries<>();
+
+
+
         for(int x=0; x<11; x++){
             double out = (double)data[x];
             series.appendData(new DataPoint(time,out),true
                     ,100);
             time++;
+
         }
         graph.addSeries(series);
     }
 
 
-}
+}*/
